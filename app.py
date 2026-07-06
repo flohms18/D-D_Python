2976af63e111162b8cb36c7cf26d28be6d83c604
flohms18 <trouilletflorent@outlook.com>
2026-07-06 18:55:19 +0200

    Add title

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-    'first column' : ['1,2,3,4'],
-    'second column' : ['10,20,30,40']
-})
 
-df
+st.title("Pandry",text_alignment="center")
+
