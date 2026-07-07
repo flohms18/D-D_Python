84f7d587a6c1c3203b1e7cfd3121192584c92316
flohms18 <trouilletflorent@outlook.com>
2026-07-07 16:25:09 +0200

    Remove Drop NaN values

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import pandas as pd
+import os
 
-st.title("Pandry",text_alignment="center")
+st.title("Pandry")
 
+
+'''
+upload_file = st.file_uploader("Pick a CSV file", type=["csv"])
+
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.write(df)
+'''
+
+df = pd.read_csv("test_data.csv")
+st.header("What to do with CSV file?")
+
+RemoveNan = st.checkbox("Remove the NaN values?")
+
+if RemoveNan:
+    new_df = df.dropna()
+    st.write(new_df)
